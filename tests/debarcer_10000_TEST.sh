e647d338e117d9a063064e327adfd1f84a86425f
bgfritz1 <bgfritz@sund.ku.dk>
2019-04-05 16:33:16 +0200

    make repo for tobias' version

---
+++ tests/debarcer_10000_TEST.sh
@@ -0,0 +1,22 @@
+#This is a script to debug debarcer and figure out why it's taking to long to run
+
+module load tools anaconda3/4.4.0
+
+
+#group UMIs
+
+#python /home/projects/ku_10025/apps/debarcer_tobias/debarcer/debarcer.py group -o /home/projects/ku_10025/apps/debarcer_tobias/tests/out -r chr1:4776044-4780817 -c /home/projects/ku_10025/apps/debarcer_tobias/config/tim_config.ini
+
+#Collapse
+#python /home/projects/ku_10025/apps/debarcer_tobias/debarcer/debarcer.py collapse -o /home/projects/ku_10025/apps/debarcer_tobias/tests/out -r chr1:4776044-4780817 -c /home/projects/ku_10025/apps/debarcer_tobias/config/tim_config.ini
+
+
+#Call VCFs
+python /home/projects/ku_10025/apps/debarcer_tobias/debarcer/debarcer.py call -o /home/projects/ku_10025/apps/debarcer_tobias/tests/out -r chr1:4776044-4780817 -f 1,2,5 -cf ./out/chr1:4776044-4780817.cons  
+
+
+
+
+
+
+
