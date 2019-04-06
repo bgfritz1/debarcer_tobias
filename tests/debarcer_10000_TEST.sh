371b154d7829a3e3bcf87de5681457c4fbeb8a66
bgfritz1 <bgfritz@sund.ku.dk>
2019-04-06 13:34:44 +0200

    update test script

--- tests/debarcer_10000_TEST.sh
+++ tests/debarcer_10000_TEST.sh
@@ -5,14 +5,14 @@ module load tools anaconda3/4.4.0
 
 #group UMIs
 
-python /home/projects/ku_10025/apps/debarcer_tobias/debarcer/debarcer.py group -o /home/projects/ku_10025/apps/debarcer_tobias/tests/out -r chr1:4776044-4780817 -c /home/projects/ku_10025/apps/debarcer_tobias/config/tim_config.ini
+#python /home/projects/ku_10025/apps/debarcer_tobias/debarcer/debarcer.py group -o /home/projects/ku_10025/apps/debarcer_tobias/tests/out -r chr1:4776044-4780817 -c /home/projects/ku_10025/apps/debarcer_tobias/config/tim_config.ini
 
 #Collapse
-python /home/projects/ku_10025/apps/debarcer_tobias/debarcer/debarcer.py collapse -o /home/projects/ku_10025/apps/debarcer_tobias/tests/out -r chr1:4776044-4780817 -c /home/projects/ku_10025/apps/debarcer_tobias/config/tim_config.ini
+#python /home/projects/ku_10025/apps/debarcer_tobias/debarcer/debarcer.py collapse -o /home/projects/ku_10025/apps/debarcer_tobias/tests/out -r chr1:4776044-4780817 -c /home/projects/ku_10025/apps/debarcer_tobias/config/tim_config.ini
 
 
 #Call VCFs
-python /home/projects/ku_10025/apps/debarcer_tobias/debarcer/debarcer.py call -o /home/projects/ku_10025/apps/debarcer_tobias/tests/out -r chr1:4776044-4780817 -f 1,2,5 -cf ./out/chr1:4776044-4780817.cons -c /home/projects/ku_10025/apps/debarcer_tobias/config/tim_config.ini 
+#python /home/projects/ku_10025/apps/debarcer_tobias/debarcer/debarcer.py call -o /home/projects/ku_10025/apps/debarcer_tobias/tests/out -r chr1:4776044-4780817 -f 1,2,5 -cf ./out/chr1:4776044-4780817.cons -c /home/projects/ku_10025/apps/debarcer_tobias/config/tim_config.ini 
 
 #Make plots 
 
