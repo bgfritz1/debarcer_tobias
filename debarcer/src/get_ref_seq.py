d588cf978a05410a96683476512cf74ec992fe31
Blaine Fritz <bgfritz@sund.ku.dk>
2019-04-06 13:58:52 +0200

    Update get_ref_seq.py
    removed debug tests

--- debarcer/src/get_ref_seq.py
+++ debarcer/src/get_ref_seq.py
@@ -3,8 +3,7 @@ import pysam
 
 def get_ref_seq(contig, region_start, region_end, config):
     """Gets reference sequence on a region from reference file."""
-    print(contig,region_start,region_end,config)
-    print(config['PATHS']['reference_file'])
+    
     with pysam.FastaFile(config['PATHS']['reference_file']) as reader:
         ref_seq = reader.fetch(contig, region_start, region_end).upper()
 
