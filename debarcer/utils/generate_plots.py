4da6d922b3c74e1b0d7edd067a48fea50e0e67b9
Blaine Fritz <bgfritz@sund.ku.dk>
2019-05-17 22:36:58 +0200

    Update generate_plots.py

--- debarcer/utils/generate_plots.py
+++ debarcer/utils/generate_plots.py
@@ -66,8 +66,7 @@ def graph_ref_freq(df, output_path, file_stem):
 		print(x)
 
 	plot = ggplot(aes('base_pos', 'ref_freq'), data=df) + \
-		geom_bar(aes(colour='f_size')) + \
-		geom_bar()+ \
+		geom_bar() + \
 		facet_wrap('f_size', ncol=1) + \
 		scale_x_continuous(breaks=range(base_pos_min, base_pos_max, base_pos_step)) + \
 		scale_y_continuous(breaks=[0.0, 25.0, 50.0, 75.0, 100.0]) + \
